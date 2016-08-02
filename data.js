83762f03c76add6dc18139043cfd5af7d03ec8c4
krmfla <krmgosh0404@hotmail.com.tw>
2016-08-02 02:08:07 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,386 @@
+var source = [
+    { "name": "物件導向概念-訂書系統",
+      "tag" : "mix",
+      "src" : "https://jsfiddle.net/krmfla/5o3hha0m/embedded/result,js/"
+    },
+    { "name": "任務管理器",
+      "tag" : "mix",
+      "src" : "https://jsfiddle.net/krmfla/bk2cLdgt/11/embedded/result,js/"
+    },
+    { "name": "中英翻譯對照",
+      "tag" : "mix",
+      "src" : "https://jsfiddle.net/krmfla/d4b9wkLx/6/embedded/result,js/"
+    },
+    { "name": "乒乓球+滑鼠點擊/滑過效果",
+      "tag" : "mix",
+      "src" : "https://jsfiddle.net/krmfla/wcf2cqcw/5/embedded/result,js/"
+    },
+    { "name": "發牌效果",
+      "tag" : "mix",
+      "src" : "https://jsfiddle.net/krmfla/qL1sytrt/1/embedded/result,js/"
+    },
+    { "name": "OOP + Flux設計模式練習",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/m7r1t2sy/6/embedded/result,js/"
+    },
+    { "name": "卡片翻轉效果",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/q8r8543v/7/embedded/result,css/"
+    },
+    { "name": "no JQuery",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/u850twfb/1/embedded/js/"
+    },
+    { "name": "番茄工作法",
+      "tag" : "mix",
+      "src" : "//jsfiddle.net/krmfla/w754p2gg/18/embedded/result,js/"
+    },
+    { "name": "按讚Module",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/c0qk6jev/6/embedded/result,js/"
+    },
+    { "name": "彈跳動畫transition",
+      "tag" : "mix",
+      "src" : "//jsfiddle.net/krmfla/1ooya597/2/embedded/result,js/"
+    },
+    { "name": "垂直水平置中",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/3ajL943s/1/embedded/result,html,css/"
+    },
+    { "name": "旅遊APP(藍)",
+      "tag" : "ui",
+      "src" : "//jsfiddle.net/krmfla/azuvnyre/9/embedded/result/"
+    },
+    { "name": "循環動畫",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/o33og5zm/3/embedded/result,js/"
+    },
+    { "name": "用偽元素做標籤",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/gap7ku91/6/embedded/result,html,css/"
+    },
+    { "name": "圓形遮罩切換動畫",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/qnvt6j6r/4/embedded/result/"
+    },
+    { "name": "用偽元素做check box",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/00mq4qLp/embedded/result,css/"
+    },
+    { "name": "OOP練習 + toElement",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/sxq7vLav/1/embedded/result,js/"
+    },
+    { "name": "長按ok鍵",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/48586wos/1/embedded/result/"
+    },
+    { "name": "咖啡色按鈕",
+      "tag" : "ui",
+      "src" : "//jsfiddle.net/krmfla/w7ucpw4u/1/embedded/result/"
+    },
+    { "name": "RWD flip切換",
+      "tag" : "mix",
+      "src" : "//jsfiddle.net/krmfla/dqw1vqrg/6/embedded/result,js/"
+    },
+    { "name": "map & each",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/ecd1tp5c/1/embedded/result,js/"
+    },
+    { "name": "Flux概念試做",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/fwr00sL1/7/embedded/result,js/"
+    },
+    { "name": "Closure試做",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/yr8q2k1x/4/embedded/js/"
+    },
+    { "name": "底線transition",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/ftj9xesn/6/embedded/result,css/"
+    },
+    { "name": "JQuery滑動效果",
+      "tag" : "mix",
+      "src" : "//jsfiddle.net/krmfla/fg5ou9q6/3/embedded/result/"
+    },
+    { "name": "避免觸發reflow",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/d38ro379/5/embedded/result,css/"
+    },
+    { "name": "數字轉樣式",
+      "tag" : "mix",
+      "src" : "//jsfiddle.net/krmfla/a0y0ao8o/1/embedded/result,js/"
+    },
+    { "name": "clamp.js-多行內容省略",
+      "tag" : "mix",
+      "src" : "//jsfiddle.net/krmfla/ubwu2L29/embedded/result/"
+    },
+    { "name": "RWD flip box",
+      "tag" : "mix",
+      "src" : "//jsfiddle.net/krmfla/y7jb3sob/3/embedded/result,js/"
+    },
+    { "name": "RWD 柵格系統",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/6Lncxpc4/embedded/result/"
+    },
+    { "name": "RWD 動畫效果",
+      "tag" : "mix",
+      "src" : "//jsfiddle.net/krmfla/juxogxa5/2/embedded/result,js/"
+    },
+    { "name": "太空子彈",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/e9oy9ddu/1/embedded/result,js/"
+    },
+    { "name": "VOD列表",
+      "tag" : "react",
+      "src" : "//jsfiddle.net/krmfla/z9utsjwz/8/embedded/result,html/"
+    },
+    { "name": "Methods & Lifecycle",
+      "tag" : "react",
+      "src" : "//jsfiddle.net/krmfla/dxbbz6cr/1/embedded/result/"
+    },
+    { "name": "RX-93-ν2 Hi-ν Gundam",
+      "tag" : "mix",
+      "src" : "https://jsfiddle.net/krmfla/j19et01L/1/embedded/result,js/"
+    },
+    { "name": "垂直跑馬燈模組",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/xe8t899c/5/embedded/result,js/"
+    },
+    { "name": "用偽元素自定ol樣式",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/gdbnsge7/embedded/result,css/"
+    },
+    { "name": "react練習4 - 計時器",
+      "tag" : "react",
+      "src" : "//jsfiddle.net/krmfla/r8r25zzh/2/embedded/result,html/"
+    },
+    { "name": "文字滑入動畫",
+      "tag" : "ui",
+      "src" : "//jsfiddle.net/krmfla/7ez3pfr1/3/embedded/result/"
+    },
+    { "name": "多行文字垂直置中",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/yjqk6288/3/embedded/result,css,html/"
+    },
+    { "name": "樹狀圖",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/18utso1d/embedded/result/"
+    },
+    { "name": "仿bootstrap樣式",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/7u6hLtdc/3/embedded/result,css/"
+    },
+    { "name": "canvas + 動畫",
+      "tag" : "mix",
+      "src" : "//jsfiddle.net/krmfla/v38L488r/1/embedded/result,js/"
+    },
+    { "name": "遮罩換色切換",
+      "tag" : "mix",
+      "src" : "//jsfiddle.net/krmfla/xdaub6sv/embedded/result,js/"
+    },
+    { "name": "手機版popup",
+      "tag" : "mix",
+      "src" : "//jsfiddle.net/krmfla/ra84f9qo/2/embedded/result/"
+    },
+    { "name": "100 Days UI",
+      "tag" : "ui",
+      "src" : "//jsfiddle.net/krmfla/m2vcL7a6/11/embedded/result/"
+    },
+    { "name": "react練習3",
+      "tag" : "react",
+      "src" : "//jsfiddle.net/krmfla/onhkmcg5/2/embedded/result,html/"
+    },
+    { "name": "側邊欄UI",
+      "tag" : "ui",
+      "src" : "//jsfiddle.net/krmfla/gptzfmcu/embedded/result/"
+    },
+    { "name": "react練習 nav menu",
+      "tag" : "react",
+      "src" : "//jsfiddle.net/krmfla/ovtvajs4/2/embedded/result,html/"
+    },
+    { "name": "let yourself in",
+      "tag" : "ui",
+      "src" : "//jsfiddle.net/krmfla/2fz4pg1d/4/embedded/result,html,css/"
+    },
+    { "name": "if else 簡寫 + 帶參數",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/fy2vfe3a/3/embedded/js/"
+    },
+    { "name": "css:before+:after",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/rumnzzpz/embedded/result,css/"
+    },
+    { "name": "new img listener",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/qn5boepa/embedded/result,js/"
+    },
+    { "name": "ad九宮格",
+      "tag" : "mix",
+      "src" : "//jsfiddle.net/krmfla/qy3nLLuw/1/embedded/result,html,css/"
+    },
+    { "name": "oop練習",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/sLvzsnpp/4/embedded/result,js/"
+    },
+    { "name": "陣列控制timeout",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/a4dg8b7t/1/embedded/result,js/"
+    },
+    { "name": "圓餅圖",
+      "tag" : "mix",
+      "src" : "//jsfiddle.net/krmfla/75h5oyox/4/embedded/result,js/"
+    },
+    { "name": "陣列Running",
+      "tag" : "mix",
+      "src" : "//jsfiddle.net/krmfla/gzrhsk5a/5/embedded/result,js/"
+    },
+    { "name": "捲動畫面，超過自訂高度",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/6adn1sky/5/embedded/result,js/"
+    },
+    { "name": "陣列增減排序",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/u340yj0k/2/embedded/result/"
+    },
+    { "name": "社群APP",
+      "tag" : "ui",
+      "src" : "//jsfiddle.net/krmfla/7vhdv2aw/10/embedded/result/"
+    },
+    { "name": "英文學習APP",
+      "tag" : "mix",
+      "src" : "//jsfiddle.net/krmfla/3ffjf5wy/18/embedded/result,js/"
+    },
+    { "name": "react練習",
+      "tag" : "react",
+      "src" : "//jsfiddle.net/krmfla/jpqxvrdu/8/embedded/result,html/"
+    },
+    { "name": "操作DOM el",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/9tjut3u6/embedded/result,js/"
+    },
+    { "name": "position的關聯",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/cLxa7h3n/1/embedded/result,css/"
+    },
+    { "name": "window.onerror",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/exxh0dtn/1/embedded/result,js/"
+    },
+    { "name": "文字外框",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/mwcpef0j/embedded/result,css/"
+    },
+    { "name": "皇帝撲克",
+      "tag" : "mix",
+      "src" : "//jsfiddle.net/krmfla/oekq7ukp/embedded/result,js/"
+    },
+    { "name": "cookie, storage",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/8vyy17a7/3/embedded/result,js/"
+    },
+    { "name": "排序",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/9tru2j9b/2/embedded/js/"
+    },
+    { "name": "JQuery用data選取",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/sywatxfc/3/embedded/result,html/"
+    },
+    { "name": "table-cell填滿剩餘寬度",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/5jszkb12/1/embedded/result,html,css/"
+    },
+    { "name": "小鍵盤切換",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/6k4nn1ao/2/embedded/result,js/"
+    },
+    { "name": "計時器練習",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/vmk9xaou/3/embedded/result,html/"
+    },
+    { "name": "seekbar",
+      "tag" : "ui",
+      "src" : "//jsfiddle.net/krmfla/ejtxs352/1/embedded/result,css/"
+    },
+    { "name": "直條圖",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/kkz7721c/18/embedded/result,html/"
+    },
+    { "name": "設置focus",
+      "tag" : "test",
+      "src" : "//jsfiddle.net/krmfla/z8awp4s5/1/embedded/result,js/"
+    },
+    { "name": "常用函數",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/fpnamn7e/4/embedded/result,js/"
+    },
+    { "name": "彈珠",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/9p7dqnrr/12/embedded/result,html/"
+    },
+    { "name": "文字圖片垂直置中",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/j96euf2t/1/embedded/result,css/"
+    },
+    { "name": "list結構",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/nrfnojrg/embedded/result,css/"
+    },
+    { "name": "單欄置中, 多欄平均分配",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/jxhcgodt/embedded/result,html,css/"
+    },
+    { "name": "載入動畫",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/rLrh3Lxf/embedded/result,css/"
+    },
+    { "name": "跑馬燈",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/tukd0hqc/6/embedded/result,js/"
+    },
+    { "name": "選取後放大",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/jtf7fkeq/embedded/result,css/"
+    },
+    { "name": "卡到物件破版",
+      "tag" : "test",
+      "src" : "//jsfiddle.net/krmfla/o0av8o1x/embedded/result/"
+    },
+    { "name": "文字垂直置中",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/0jys5ffh/2/embedded/result,css/"
+    },
+    { "name": "自動填滿剩餘寬度",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/g2yqjpa9/2/embedded/result,css/"
+    },
+    { "name": "垂直置中",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/6agdan21/1/embedded/result,css/"
+    },
+    { "name": "用JSON做list",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/n8f64f77/6/embedded/result,js/"
+    },
+    { "name": "用按鍵切台",
+      "tag" : "js",
+      "src" : "//jsfiddle.net/krmfla/21v4e9t9/9/embedded/result,js/"
+    },
+    { "name": "pure css UI",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/0cxb1fk8/7/embedded/result,css/"
+    },
+    { "name": "用clearfix,撐開父元素",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/k7sm45mo/1/embedded/result,css/"
+    },
+    { "name": "圖片倒影漸層",
+      "tag" : "css",
+      "src" : "//jsfiddle.net/krmfla/0oj82m7y/5/embedded/result,css/"
+    },
+    { "name": "單選選單",
+      "tag" : "test",
+      "src" : "//jsfiddle.net/krmfla/t4ab7h5h/7/embedded/result,html/"
+    }
+];
