651573b096d439ea332a5802764e748afae38a08
Red Chou <krmgosh0404@hotmail.com.tw>
2016-10-03 11:04:21 +0800

    Create debug.js

---
+++ example/debug-js/debug.js
@@ -0,0 +1,53 @@
+function Debug() {
+  var timer = null;
+  var timerEnd = null;
+  var bodyEl = document.getElementsByTagName("body")[0];
+  
+  var targetEl = document.createElement("DIV");
+  targetEl.style.position = "absolute";
+  targetEl.style.left = 0;
+  targetEl.style.bottom = 0;
+  targetEl.style.color = "gray";
+  targetEl.style.background = "black";
+  targetEl.style.minHeight = "100vh";
+
+  bodyEl.appendChild(targetEl);
+  
+  //ONWINDOW ERROR
+  window.onerror = function (errorMsg, url, lineNumber) {
+    log('Error: ' + errorMsg);
+    log('Script: ' + url);
+    log('Line: ' + lineNumber);   
+  };
+  
+  function log(text) {
+    var element = document.createElement("P");
+    element.style.margin = 0;
+    element.style.marginBottom = "5px";
+    element.innerText = text;
+    targetEl.appendChild(element);
+  }
+  
+  function setTimer() {
+    timer = Date.now();
+    log(">>> Timer Start >>>");
+  }
+  
+  function endTimer() {
+    if (timer === null) {
+      log(">>> Timer was not set yet >>>");
+      return;
+    } else {
+      timerEnd = Date.now();
+      log(">>> process: " + (timerEnd - timer) + "ms >>>");
+      timer = null;
+    }
+  }
+  
+  //API export
+  return {
+    "log": log,
+    "time": setTimer,
+    "end": endTimer
+  };
+}
