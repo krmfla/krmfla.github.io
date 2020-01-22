363462e29c2436bde071aea3b875a218376e43ef
Red Chou <red.chou@spring-trees.com>
2020-01-22 14:54:03 +0800

    add gashapon

---
+++ example/works/script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+    cssMode: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+});
