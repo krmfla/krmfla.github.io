5425d0256325b596d2e53447b534b6691e0e4080
Red Chou <red.chou@spring-trees.com>
2024-08-23 15:00:53 +0800

    remove gj

--- example/gj/js/sample.js
+++
@@ -1,1 +0,0 @@
-"use strict";var app=new Vue({el:"#app",data:function(){return{initText:"SAMPLE VIEW",selectOpt:"",selectLists:[{text:"opts",value:""},{text:"opt1",value:1},{text:"opt2",value:2},{text:"opt3",value:3}]}},methods:{},mounted:function(){loadpage("init",{async:!0})}});
